eaffe6c9fe51ab4e61f559459fe808bc1bb991b5
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-04-04 12:45:44 +0000

    code refactor . changed app name to Jaspic

---
+++ app/src/main/java/com/mcdev/jaspic/MainActivity.java
@@ -0,0 +1,42 @@
+package com.mcdev.jaspic;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = MainActivity.this.getClass().getSimpleName();
+    private final int requestCode = 1049;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout layout = findViewById(R.id.activity_main);
+        layout.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent,requestCode);
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MainActivity.this.requestCode && resultCode == RESULT_OK) {
+            Log.d(TAG, "onActivityResult: result ok");
+            Uri uri = data.getData();
+
+            Intent intent = new Intent(getApplicationContext(), CropActivity.class);
+            intent.setData(uri);
+            startActivity(intent);
+        }
+    }
+}
